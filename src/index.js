932a15365de1dc6c5956d8623db2e10dbd8f088f
Lau Skeeter <lawr@lauragonzalez.cc>
2017-07-21 22:46:15 +0200

    hacked together a canvas renderer
    code is super ugly :(

--- src/index.js
+++ src/index.js
@@ -77,3 +77,119 @@ $('#addDonut').on('click',()=>addDonut());
 $('#addStore').on('click',()=>addStore());
 $('#addBank').on('click',()=>addBank());
 $('#redraw').on('click',()=>redraw());
+
+
+const $img = $(`<img src=${require('images/floor.png')}>`);
+const $cursor = $(`<img src=${require('images/cursor.png')}>`);
+const $factory = $(`<img src=${require('images/factory.png')}>`);
+
+$('body').append($img).append($cursor);
+
+$img.on('load',()=>{
+    var canvas = document.getElementById('play');
+    var ctx = canvas.getContext('2d');
+
+    let scale = 2;
+
+    ctx.canvas.width  = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    ctx.font = "8px Verdana";
+    ctx.scale(scale, scale);
+    ctx.mozImageSmoothingEnabled = false;
+    ctx.webkitImageSmoothingEnabled = false;
+    ctx.imageSmoothingEnabled = false;
+
+    const cols = 20;
+    const rows = cols*1.5;
+
+    const tileDimensions = [32,16]
+
+    let offset = [0,0]
+
+    let mousedown = false;
+    let mousedrag = false;
+    let mousedragOrigin = [0,0];
+    let cursorAt = [0,0];
+
+    canvas.addEventListener('mousedown',ev=>{
+        mousedown = true;
+        mousedragOrigin = cursorAt;
+    });
+    canvas.addEventListener('mouseup',ev=>{
+        mousedown = false;
+    });
+    canvas.addEventListener('mousemove',ev=>{
+        cursorAt = [ev.x/scale,ev.y/scale];
+        if(mousedown) {
+            offset = [
+                offset[0]+cursorAt[0]-mousedragOrigin[0],
+                offset[1]+cursorAt[1]-mousedragOrigin[1]
+            ];
+            mousedragOrigin = cursorAt;
+        }
+    });
+
+    const getPixelsFromTile = (x,y,height=tileDimensions[1]) => {
+        return [
+            tileDimensions[0]*x + (tileDimensions[0]*(rows-y)/2) + offset[0],
+            tileDimensions[1]*(y/2) + offset[1] + (height/2-tileDimensions[1]/2)
+        ]
+    }
+    const getTileFromPixels = (x,y) => {
+
+        x = x - offset[0];
+        y = y - offset[1];
+
+        y = y / (tileDimensions[1]/2);
+
+        x = x + (tileDimensions[0]/2*((rows-y)*-1))
+        x = x / (tileDimensions[0]);
+
+        x = x - .33;
+        y = y - .25;
+
+        x = Math.floor(x);
+        y = Math.floor(y);
+
+        return [x,y];
+    }
+
+    const draw = () => {
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // floor pass
+        for(let yIndex=0;yIndex < rows;yIndex++) {
+            for(let xIndex=0;xIndex < cols;xIndex++) {
+                let tilePosAt = getPixelsFromTile(xIndex,yIndex);
+                ctx.drawImage(
+                    $img[0],tilePosAt[0],tilePosAt[1]
+                );
+            }
+        }
+
+        // cursor pass
+        const cursorPosAt = getPixelsFromTile(...getTileFromPixels(...cursorAt));
+
+        ctx.drawImage(
+            $cursor[0],cursorPosAt[0],cursorPosAt[1]
+        );
+
+        // store pass
+        ctx.drawImage(
+            $factory[0],...getPixelsFromTile(2,8,32)
+        );
+    };
+
+    const drawLoop = () => {
+        requestAnimationFrame(()=>{
+            draw();
+            drawLoop();
+        })
+    }
+
+    drawLoop();
+
+
+
+})
