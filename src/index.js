9c20ce063ef2dc8a82b8fe9e68dedbfce62ecc1b
Lau Skeeter <lawr@lauragonzalez.cc>
2017-07-22 12:40:40 +0200

    refactor draw code into its own file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import { MONEY, DONUT } from 'resources.js'
+import { MONEY, DONUT } from 'resources'
+import { paint } from 'paint'
 
 import $ from 'jquery';
-import Factory from './objects/factory';
-import Store from './objects/store';
-import Bank from './objects/bank';
+import Factory from 'objects/factory';
+import Store from 'objects/store';
+import Bank from 'objects/bank';
 
 import './styles/index.scss'
 
@@ -15,6 +16,7 @@ const store = {
 
 const redraw = () => {
 
+    return;
     store.items.map(item => {
 
         let $item = $canvas.find('#'+item.uuid);
@@ -69,129 +71,12 @@ const addBank = () => {
     redraw();
 }
 
-setInterval(redraw,1000);
+document.querySelector('#addDonut').addEventListener('click',()=>addDonut());
+document.querySelector('#addStore').addEventListener('click',()=>addStore());
+document.querySelector('#addBank').addEventListener('click',()=>addBank());
+document.querySelector('#redraw').addEventListener('click',()=>redraw());
 
-window.store = store;
-
-$('#addDonut').on('click',()=>addDonut());
-$('#addStore').on('click',()=>addStore());
-$('#addBank').on('click',()=>addBank());
-$('#redraw').on('click',()=>redraw());
-
-
-const $img = $(`<img src=${require('images/floor.png')}>`);
-const $cursor = $(`<img src=${require('images/cursor.png')}>`);
-const $factory = $(`<img src=${require('images/factory.png')}>`);
-
-$('body').append($img).append($cursor);
-
-$img.on('load',()=>{
-    var canvas = document.getElementById('play');
-    var ctx = canvas.getContext('2d');
-
-    let scale = 2;
-
-    ctx.canvas.width  = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
-    ctx.font = "8px Verdana";
-    ctx.scale(scale, scale);
-    ctx.mozImageSmoothingEnabled = false;
-    ctx.webkitImageSmoothingEnabled = false;
-    ctx.imageSmoothingEnabled = false;
-
-    const cols = 20;
-    const rows = cols*1.5;
-
-    const tileDimensions = [32,16]
-
-    let offset = [0,0]
-
-    let mousedown = false;
-    let mousedrag = false;
-    let mousedragOrigin = [0,0];
-    let cursorAt = [0,0];
-
-    canvas.addEventListener('mousedown',ev=>{
-        mousedown = true;
-        mousedragOrigin = cursorAt;
-    });
-    canvas.addEventListener('mouseup',ev=>{
-        mousedown = false;
-    });
-    canvas.addEventListener('mousemove',ev=>{
-        cursorAt = [ev.x/scale,ev.y/scale];
-        if(mousedown) {
-            offset = [
-                offset[0]+cursorAt[0]-mousedragOrigin[0],
-                offset[1]+cursorAt[1]-mousedragOrigin[1]
-            ];
-            mousedragOrigin = cursorAt;
-        }
-    });
-
-    const getPixelsFromTile = (x,y,height=tileDimensions[1]) => {
-        return [
-            tileDimensions[0]*x + (tileDimensions[0]*(rows-y)/2) + offset[0],
-            tileDimensions[1]*(y/2) + offset[1] - (height*1.5-tileDimensions[1]*1.5)
-        ]
-    }
-    const getTileFromPixels = (x,y) => {
-
-        x = x - offset[0];
-        y = y - offset[1];
-
-        y = y / (tileDimensions[1]/2);
-
-        x = x + (tileDimensions[0]/2*((rows-y)*-1))
-        x = x / (tileDimensions[0]);
-
-        x = x - .33;
-        y = y - .25;
-
-        x = Math.floor(x);
-        y = Math.floor(y);
-
-        return [x,y];
-    }
-
-    const draw = () => {
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        // floor pass
-        for(let yIndex=0;yIndex < rows;yIndex++) {
-            for(let xIndex=0;xIndex < cols;xIndex++) {
-                let tilePosAt = getPixelsFromTile(xIndex,yIndex);
-                ctx.drawImage(
-                    $img[0],tilePosAt[0],tilePosAt[1]
-                );
-            }
-        }
-
-        // cursor pass
-        const cursorPosAt = getPixelsFromTile(...getTileFromPixels(...cursorAt));
-
-        ctx.drawImage(
-            $cursor[0],cursorPosAt[0],cursorPosAt[1]
-        );
-
-        // store pass
-        store.items.map((item,index) => {
-            ctx.drawImage(
-                $factory[0],...getPixelsFromTile(index,index,32)
-            );
-        });
-    };
-
-    const drawLoop = () => {
-        requestAnimationFrame(()=>{
-            draw();
-            drawLoop();
-        })
-    }
-
-    drawLoop();
-
-
-
-})
+const canvas = document.getElementById('play');
+paint(canvas,{
+    store: store
+});
--- src/paint.js
+++ src/paint.js
@@ -1,5 +1,127 @@
-export const paint(ctx,options={}) {
+const cols = 20;
+const rows = cols*1.5;
 
-	
+const tileDimensions = [32,16]
+
+let offset = [0,0]
+
+let mousedown = false;
+let mousedrag = false;
+let mousedragOrigin = [0,0];
+let cursorAt = [0,0];
+let scale = 2;
+
+var grayscale = function() {
+  for (var i = 0; i < data.length; i += 4) {
+	var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+	data[i]     = avg; // red
+	data[i + 1] = avg; // green
+	data[i + 2] = avg; // blue
+  }
+  ctx.putImageData(imageData, 0, 0);
+};
+
+const imageList = (()=>{
+    let rt = {};
+    [
+        'floor','cursor',
+        'factory','store','bank'
+    ].map(image => {
+        rt[image] = new Image();
+        rt[image].src = require(`images/${image}.png`)
+    });
+    return rt;
+})();
+
+const getPixelsFromTile = (x,y,height=tileDimensions[1]) => {
+	return [
+		tileDimensions[0]*x + (tileDimensions[0]*(rows-y)/2) + offset[0],
+		tileDimensions[1]*(y/2) + offset[1] - (height*1.5-tileDimensions[1]*1.5)
+	]
+}
+
+const getTileFromPixels = (x,y) => {
+
+	x = x - offset[0];
+	y = y - offset[1];
+
+	y = y / (tileDimensions[1]/2);
+
+	x = x + (tileDimensions[0]/2*((rows-y)*-1))
+	x = x / (tileDimensions[0]);
+
+	x = x - .33;
+	y = y - .25;
+
+	x = Math.floor(x);
+	y = Math.floor(y);
+
+	return [x,y];
+}
+
+export const paint = (canvas,{store={}}={}) => {
+
+	const ctx = canvas.getContext('2d');
+
+	ctx.canvas.width  = window.innerWidth;
+	ctx.canvas.height = window.innerHeight;
+	ctx.font = "8px Verdana";
+	ctx.scale(scale, scale);
+	ctx.imageSmoothingEnabled = false;
+
+	canvas.addEventListener('mousedown',ev=>{
+	    mousedown = true;
+	    mousedragOrigin = cursorAt;
+	});
+	canvas.addEventListener('mouseup',ev=>{
+	    mousedown = false;
+	});
+	canvas.addEventListener('mousemove',ev=>{
+	    cursorAt = [ev.x/scale,ev.y/scale];
+	    if(mousedown) {
+	        offset = [
+	            offset[0]+cursorAt[0]-mousedragOrigin[0],
+	            offset[1]+cursorAt[1]-mousedragOrigin[1]
+	        ];
+	        mousedragOrigin = cursorAt;
+	    }
+	});
+
+	const draw = () => {
+
+	    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	    // floor pass
+	    for(let yIndex=0;yIndex < rows;yIndex++) {
+	        for(let xIndex=0;xIndex < cols;xIndex++) {
+	            let tilePosAt = getPixelsFromTile(xIndex,yIndex);
+	            ctx.drawImage(
+	                imageList.floor,tilePosAt[0],tilePosAt[1]
+	            );
+	        }
+	    }
+
+	    // cursor pass
+	    let cursorPosAt = getPixelsFromTile(...getTileFromPixels(...cursorAt));
+	    ctx.drawImage(
+	        imageList.cursor,cursorPosAt[0],cursorPosAt[1]
+	    );
+
+	    // store pass
+	    store.items.map((item,index) => {
+	        ctx.drawImage(
+	            imageList[item.type],...getPixelsFromTile(index,index,32)
+	        );
+	    });
+	};
+
+	const drawLoop = () => {
+	    requestAnimationFrame(()=>{
+	        draw();
+	        drawLoop();
+	    })
+	}
+
+	drawLoop();
 
 }
